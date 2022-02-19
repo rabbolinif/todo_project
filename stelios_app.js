d0746ddc53efdad3221c9d100b953d1958a9bf7c
Stelios Rusta <steliosrusta@gmail.com>
2022-02-19 16:10:50 +0100

    Start Js

--- stelios_app.js
+++ stelios_app.js
@@ -1,1 +1,33 @@
-const task = document.querySelector(".task");
+const listsContainer = document.querySelector(".all-lists");
+
+let myLists = ["hi", "two"];
+
+class List {
+  constructor(item) {
+    this.ulWithTasks = item;
+  }
+  addList() {
+    const listName = document.querySelector("[data-add-list-name]").value;
+    if (!listName) alert("invalid input");
+
+    const listItem = {
+      name: listName,
+      id: myLists.length,
+    };
+    myLists.push(listItem);
+    listItem.rend();
+    document.querySelector("[data-add-list-name]").value = "";
+  }
+  rend() {
+    listsContainer.innerHTML = "";
+
+    myLists.forEach((list) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("contenteditable", "true");
+      listItem.classList.add("list-name");
+      listItem.innerText = list;
+      listsContainer.appendChild(listItem);
+    });
+  }
+}
+new List().rend();
